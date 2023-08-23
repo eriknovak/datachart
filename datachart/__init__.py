5e6ce12018e44a806562171b8e2828203a216219
Erik Novak <erik.novak@ijs.si>
2023-08-23 18:52:36 +0200

    :bug: Fix the chart package import

--- datachart/__init__.py
+++ datachart/__init__.py
@@ -1,5 +1,21 @@
-# import visualization charts
-from .charts import *
+"""
+datachart
+=========
+
+Provides
+  1. Data visualization tools
+  2. Visualization customization
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code and a loose standing reference guide, available
+from `the datachart homepage <https://eriknovak.github.io/datachart>`.
+
+"""
+
+from . import charts
+from .charts import line_chart, bar_chart, hist_chart
 
 from . import config
 from . import schema
